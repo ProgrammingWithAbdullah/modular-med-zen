01c48a741e2433f0f714e41cddf06809b8433863
Abdullah Amer <abdullahraaajput1727@gmail.com>
2025-08-18 06:34:58 +0500

    feat: add WhyChooseUs page with detailed reasons for choosing Modular Med; includes header, footer, and various sections for user engagement

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import WhyChooseUs from "./pages/why-chose-up";
+import WhyChooseUs from "./pages/why-chose-us";
 import ProductsServices from "./pages/ProductsServices";
 import Affiliations from "./pages/Affiliations";
 import TeamMembers from "./pages/TeamMembers";
